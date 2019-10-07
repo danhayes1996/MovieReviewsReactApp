f2b8c45735709a3470f34bd6e3c6672513f1db1d
dan hayes <dan.hayes1996@gmail.com>
2019-10-07 01:10:29 +0100

    inital commit

---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+export default class HomeComponent extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            movies: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8080/movie/all')
+            .then(value => {
+                console.log(value);
+                this.setState({movies: value.data});
+            }).catch(error =>{
+                if(!error.response) {
+                    console.error("Error: Couldn't connect to API");
+                } else {
+                    console.error(error);
+                }
+            });
+    }
+
+    render(){
+        return (
+            <div className="movies">
+                {this.state.movies.map((movie, index) => {
+                    return <p key={"movie" + index}>{movie.name}</p>
+                })}
+            </div>
+        )
+    }
+}  
